794d4ca20908bdc2eada64a16df1dc1b7cf14459
Fernando Cueto <fernandoecueto@gmail.com>
2025-02-18 17:34:23 +0100

    update ServicesSection to use dynamic AOS effects for service items

--- src/components/ServicesSection.vue
+++ src/components/ServicesSection.vue
@@ -4,7 +4,7 @@
       <h2 class="text-4xl font-bold text-white mb-4">My Services</h2>
     </div>
     <div class="py-8 xl:px-16 px-4 sm:py-16 grid grid-cols-1 gap-6 pt-10 sm:grid-cols-2 md:gap-10 md:pt-12 lg:grid-cols-3">
-      <div v-for="service in services" :key="service.id" data-aos="fade-up"
+      <div v-for="service in services" :key="service.id" :data-aos="service.effect"
         class="px-8 py-12 rounded-xl bg-[#111a3e] shadow-lg border border-[#1f1641]"
       >
         <div class="mx-auto h-24 text-center xl:h-28 xl:w-28">
@@ -31,19 +31,22 @@
       id: 1,
       icon: 'https://img.icons8.com/ios-filled/100/ffffff/web-design.png',
       name: 'Frontend Development',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae accusantium amet ab, tenetur nulla mollitia magni rerum. Quia, vero hic corporis, labore neque eos, dolorem voluptatem unde doloribus modi molestias.'
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae accusantium amet ab, tenetur nulla mollitia magni rerum. Quia, vero hic corporis, labore neque eos, dolorem voluptatem unde doloribus modi molestias.',
+      effect: 'fade-right'
     },
     {
       id: 2,
       icon: 'https://img.icons8.com/ios-filled/100/ffffff/google-code.png',
       name: 'Backend Development',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae accusantium amet ab, tenetur nulla mollitia magni rerum. Quia, vero hic corporis, labore neque eos, dolorem voluptatem unde doloribus modi molestias.'
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae accusantium amet ab, tenetur nulla mollitia magni rerum. Quia, vero hic corporis, labore neque eos, dolorem voluptatem unde doloribus modi molestias.',
+      effect: 'fade-up'
     },
     {
       id: 3,
       icon: 'https://img.icons8.com/external-vectorslab-glyph-vectorslab/1000/fffeee/external-Technical-Writing-mobile-app-development-vectorslab-glyph-vectorslab.png',
       name: 'Technical Writing',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae accusantium amet ab, tenetur nulla mollitia magni rerum. Quia, vero hic corporis, labore neque eos, dolorem voluptatem unde doloribus modi molestias.'
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae accusantium amet ab, tenetur nulla mollitia magni rerum. Quia, vero hic corporis, labore neque eos, dolorem voluptatem unde doloribus modi molestias.',
+      effect: 'fade-left'
     },
   ])
 </script>
