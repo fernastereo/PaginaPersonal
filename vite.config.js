264fd02ecdd26a265a798335cff64e89d4ca435d
Fernando Cueto <fernandoecueto@gmail.com>
2025-03-07 16:26:09 +0100

    add deploy script to package.json and update Vite config for base path

--- vite.config.js
+++ vite.config.js
@@ -7,14 +7,11 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-    tailwindcss(),
-  ],
+  base: '/PaginaPersonal/',
+  plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-})
+});
