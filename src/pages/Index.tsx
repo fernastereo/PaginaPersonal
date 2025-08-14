68c75dfa4bff7774c09c4ce15fd40896c3f4d6cd
Fernando Cueto <fernandoecueto@gmail.com>
2025-08-14 23:00:52 +0200

    instala y confugura shadcn

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,5 +1,12 @@
+import { Button } from '@/components/ui/button';
+
 const Index = () => {
-  return <div>Index</div>;
+  return (
+    <div className='flex min-h-svh flex-col items-center justify-center'>
+      <p className='text-3xl font-bold underline'>Hello World</p>
+      <Button>Click me</Button>
+    </div>
+  );
 };
 
 export default Index;
