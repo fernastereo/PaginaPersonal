35e17e298a5183e4d6cd1fdf2d46eaad744fbffa
Fernando Cueto <fernandoecueto@gmail.com>
2025-08-14 22:08:26 +0200

    crea pagina de index y notFound

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './lib/i18n';
+import Index from './pages/Index';
+import NotFound from './pages/NotFound';
 
+const queryClient = new QueryClient();
 function App() {
   return (
-    <>
-      <p className='text-xl font-bold underline'>Hello World</p>
-    </>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path='/'
+            element={<Index />}
+          />
+          <Route
+            path='*'
+            element={<NotFound />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
-export default App
+export default App;
