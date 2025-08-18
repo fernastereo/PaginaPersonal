5a69b73d2c3d8876f5e07bdba2f18c70024639fb
Fernando Cueto <fernandoecueto@gmail.com>
2025-08-18 02:27:15 +0200

    actualiza el componente SocialLink para incluir un fondo oscuro en modo oscuro

--- src/components/custom-ui/SocialLink.tsx
+++ src/components/custom-ui/SocialLink.tsx
@@ -1,7 +1,7 @@
 export const SocialLink = ({ icon, href, label }: { icon: string, href: string, label: string }) => {
   return (
     <a
-      className="px-3 py-3 rounded-full hover:scale-105 hover:bg-primary/30 hover:shadow-md transition-all duration-300"
+      className="px-3 dark:bg-secondary-foreground py-3 rounded-full hover:scale-105 hover:bg-primary/30 hover:shadow-md transition-all duration-300"
       title={label}
       href={href}
       target="_blank"
@@ -9,5 +9,5 @@ export const SocialLink = ({ icon, href, label }: { icon: string, href: string, 
     >
       <img src={icon} alt={label} className="h-6 w-6" />
     </a>
-  )
+  );
 }
