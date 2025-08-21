5af8139ddd35af56a469ea23b8af1bccaa0f9a3e
Fernando Cueto <fernandoecueto@gmail.com>
2025-08-21 23:25:43 +0200

    feat: agrega dependencias de @testing-library para pruebas y configura el archivo de setup para Vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,10 +7,11 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
+    setupFiles: ['./src/test/setup.ts'],
   },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
-  }
-})
+  },
+});
